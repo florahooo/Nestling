e02579811c8038fc6b3f6ea14ffe81e026fdc371
htl3357 <86332481+htl3357@users.noreply.github.com>
2021-08-15 09:02:30 -0400

    Update pomodoro.js

--- pomodoro.js
+++ pomodoro.js
@@ -1,26 +1,38 @@
-const startingMinutes = 25; 
-let time = startingMinutes * 60; 
+const startingMinutes = 25;
+let time = startingMinutes * 60;
 const countdownM = document.getElementById("countdown");
-let pauseflag = false; 
-
+var pauseflag = false;
+var myTimer;
 
 function countdownupdate() {
-    const minutes = Math.floor(time/60); 
-    let seconds = time % 60; 
-    //let countdownM = document.getElementById("countdown");
-    if (!pauseflag) 
-    {
-        countdownM.innerHTML = `${minutes}: ${seconds}`;
-        time--; 
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  //let countdownM = document.getElementById("countdown");
+  console.log(pauseflag);
+  if (!pauseflag) {
+    countdownM.innerHTML = `${minutes}: ${seconds}`;
+    if (minutes == 0 && seconds == 0) {
+      clearInterval(myTimer);
+      console.log("clearInterval");
+      let audio = new Audio("alarm_soft.mp3");
+      audio.play();
+    } else {
+      time--;
     }
-    if (minutes == 0 && seconds == 0)
-    {
-        let audio = new Audio('alarm_soft.mp3')
-        audio.play();
-    }
-    
+  }
+}
+
+function playcountdown() {
+  if (!pauseflag && myTimer == null) {
+    myTimer = setInterval(countdownupdate, 1000);
+    console.log("setInterval")
+  } else {
+    pauseflag = false;
+  }
 }
 
 function pausecountdown() {
-
+  pauseflag = true;
+  console.log("paused");
+  console.log(pauseflag);
 }
